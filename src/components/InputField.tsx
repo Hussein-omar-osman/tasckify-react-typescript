234652cbc611a4fc1e46ae008588203f767266b0
Hussein Omar <husseinomar6190@gmail.com>
2022-09-29 22:13:52 +0300

    adding state for input value

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './styles.css';
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField = ({ todo, setTodo }: Props) => {
   return (
     <form className='input'>
-      <input type='text' placeholder='Enter a Task' className='input__box' />
+      <input
+        type='text'
+        placeholder='Enter a Task'
+        className='input__box'
+        name='todo'
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
       <button type='button' className='input_submit'>
         GO
       </button>
